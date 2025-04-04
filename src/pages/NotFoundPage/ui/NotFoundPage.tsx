327da43f3c3c95f10e17ce3649f402e8074dc96b
EndlessC0ding <endlesscoding@yahoo.com>
2025-04-04 21:52:50 +0300

    cr p/NtFndpg

---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,5 @@
+import s from "./NotFoundPage.module.scss";
+const NotFoundPage = () => {
+  return <main className={`${s.notFoundPage}`}>{"Page Not Found"}</main>;
+};
+export default NotFoundPage;
